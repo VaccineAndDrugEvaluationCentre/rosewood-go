e42166a5e5deb3cd88d9c75761a8bf2684361979
DrGo <salah.mahmud@gmail.com>
2017-09-05 21:00:52 -0500

    added testing files

---
+++ testing_const.go
@@ -0,0 +1,10 @@
+package rosewood
+
+//define global testing constants
+
+const (
+	showOutput  = true
+	writeOut    = true
+	testDirName = "test-files" //relative to the executable directory
+	testFileExt = "html"
+)
